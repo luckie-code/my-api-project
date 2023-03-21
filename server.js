767439b3147ff0ae7166f43e21d4f05e572579eb
Antai lucky <Antailacky@gmail.com>
2023-03-21 02:24:23 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'age': 30,
+        'birthName': 'Sheyaa Bin Abramham-joseph',
+        'birthLocation': 'London, England'
+},
+'chance the rapper': {
+    'age': 29,
+    'birthName': 'Chancelor Bennett',
+    'birthLocation': 'Chicago, illinois'
+},
+    'dylan':{
+        'age': 29,
+        'birthName':'dylan',
+        'birthLocation': 'dylan'
+    }
+}
+app.get('/', (request, response)=>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:rapperName',  (request, response)=>{
+    const rappersName = request.params.rapperName.toLowerCase()
+    if(rappers[rappersName]) {
+        response.json(rappers[rappersName])
+    }else {
+        response.json(rappers['dylan'])
+
+    }
+    //response.json(rappers)
+})
+
+app.listen(PORT, ()=> {
+    console.log(`The server is running on ${PORT}! You better go catch it!`)
+})
